3847f74730d123474ec666b17251db1278bb547a
kendricktan <kendricktan0814@gmail.com>
2016-09-14 22:00:23 +1000

    🦄 added throttling visualization

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # suiron
 Franklin, the machine learning rc car
+
+## Dependencies
+```
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install python-opencv python-dev
+
+pip install -r requirements.txt
+```
+
+## Collecting data
+### todo
+
+## Training data
+### todo
+
+## Visualizing collected data
+### todo
--- suiron/core/SuironVZ.py
+++ suiron/core/SuironVZ.py
@@ -1,5 +1,5 @@
-import cv2
 import numpy as np
+import cv2
 import pandas as pd
 
 WIDTH = 75
@@ -11,13 +11,19 @@ def visualize_data(filename):
 
     for i in data.index:
         cur_img = data['image'][i]
-        cur_throttle = data['throttle'][i]
+        cur_throttle = int(data['throttle'][i])
         
         # [1:-1] is used to remove '[' and ']' from string 
         cur_img_array = np.fromstring(cur_img[1:-1], sep=',', dtype='uint8')
         cur_img_array = np.resize(cur_img_array, (HEIGHT, WIDTH, DEPTH))
 
         cur_img_array = cv2.resize(cur_img_array, (480, 320), interpolation=cv2.INTER_CUBIC)
-        cv2.imshow('frame', cv2.cvtColor(cur_img_array, cv2.COLOR_RGB2BGR))
-        cv2.waitKey(1)
 
+        # Extra info
+        cv2.putText(cur_img_array, "frame: {}".format(str(i)), (5,35), cv2.FONT_HERSHEY_SIMPLEX, 1, 255)
+        cv2.line(cur_img_array, (240, 300), (240+cur_throttle, 200), (0, 255, 0), 3)
+
+        # Show frame
+        # Convert to BGR cause thats how OpenCV likes it
+        cv2.imshow('frame', cv2.cvtColor(cur_img_array, cv2.COLOR_RGB2BGR))
+        cv2.waitKey(0)
