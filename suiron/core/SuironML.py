b91e516c2bfef15446fd6d806363f9fab77d3088
kendricktan <kendricktan0814@gmail.com>
2016-09-15 22:30:23 +1000

    🍆  changed CNN architecture

--- suiron/core/SuironML.py
+++ suiron/core/SuironML.py
@@ -3,6 +3,7 @@ import tflearn
 from tflearn.data_utils import shuffle, to_categorical
 from tflearn.layers.core import input_data, dropout, fully_connected
 from tflearn.layers.conv import conv_2d, max_pool_2d
+from tflearn.layers.normalization import local_response_normalization
 from tflearn.layers.estimator import regression
 
 # Gets a 2 layered CNN
@@ -10,24 +11,35 @@ from tflearn.layers.estimator import regression
 def get_cnn_model(checkpoint_path='cnn_model', width=72, height=48, depth=3, outputs=10):
     network = input_data(shape=[None, height, width, depth], name='input')
 
-    # Convolution layer no.1
-    # ReLU was used as it introduces non-linearity
-    network = conv_2d(network, height, 3, activation='relu')
-    network = max_pool_2d(network, 2) # Dimensionality reduction and feature extraction
+    # Convolution no.1
+    # Max pooling does feature extraction
+    # Relu introduces non linearity into training
+    network = conv_2d(network, 96, 11, strides=4, activation='relu')
+    network = max_pool_2d(network, 3, strides=2)
+    network = local_response_normalization(network)
 
-    # Convolution layer no.2
-    network = conv_2d(network, height*2, 3, activation='relu')
-    network = conv_2d(network, height*2, 3, activation='relu')
-    network = max_pool_2d(network, 2)
-
-    # Feed forward/fully connected layer
-    network = fully_connected(network, 512, activation='relu')
-    network = dropout(network, 0.5) # Reduces overfitting
+    # Convolution no.2
+    network = conv_2d(network, 256, 5, activation='relu')
+    network = max_pool_2d(network, 3, strides=2)
+    network = local_response_normalization(network)
+    
+    # Convolution no.3
+    network = conv_2d(network, 384, 3, activation='relu')
+    network = conv_2d(network, 384, 3, activation='relu')
+    network = conv_2d(network, 256, 3, activation='relu')
+    network = max_pool_2d(network, 3, strides=2)
+    network = local_response_normalization(network)
+    network = fully_connected(network, 4096, activation='tanh')
+    # Dropout generalizes better
+    network = dropout(network, 0.5)
+    network = fully_connected(network, 4096, activation='tanh')
+    network = dropout(network, 0.5)
+    network = fully_connected(network, outputs, activation='softmax')
 
     network = fully_connected(network, outputs, activation='softmax')
-    network = regression(network, optimizer='adam', 
+    network = regression(network, optimizer='momentum', 
                         loss='categorical_crossentropy', 
-                        learning_rate=0.01, name='target')
+                        learning_rate=0.001, name='target')
 
     # Verbosity yay nay
     # 0 = nothing
