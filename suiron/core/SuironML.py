05c53f697c39d2532447d76726bc3bf239cce210
kendricktan <kendricktan0814@gmail.com>
2016-09-15 23:19:16 +1000

    ☃ tweaked CNN miodel

--- suiron/core/SuironML.py
+++ suiron/core/SuironML.py
@@ -8,35 +8,34 @@ from tflearn.layers.estimator import regression
 
 # Gets a 2 layered CNN
 # (used for unprocessed data)
-def get_cnn_model(checkpoint_path='cnn_model', width=72, height=48, depth=3, outputs=10):
+def get_cnn_model(checkpoint_path='cnn_model', width=72, height=48, depth=3, output=10):
     network = input_data(shape=[None, height, width, depth], name='input')
 
     # Convolution no.1
     # Max pooling does feature extraction
     # Relu introduces non linearity into training
-    network = conv_2d(network, 96, 11, strides=4, activation='relu')
+    network = conv_2d(network, 64, 11, strides=4, activation='relu')
     network = max_pool_2d(network, 3, strides=2)
     network = local_response_normalization(network)
 
     # Convolution no.2
-    network = conv_2d(network, 256, 5, activation='relu')
+    network = conv_2d(network, 128, 5, activation='relu')
     network = max_pool_2d(network, 3, strides=2)
     network = local_response_normalization(network)
     
     # Convolution no.3
-    network = conv_2d(network, 384, 3, activation='relu')
-    network = conv_2d(network, 384, 3, activation='relu')
     network = conv_2d(network, 256, 3, activation='relu')
+    network = conv_2d(network, 256, 3, activation='relu')
+    network = conv_2d(network, 128, 3, activation='relu')
     network = max_pool_2d(network, 3, strides=2)
     network = local_response_normalization(network)
-    network = fully_connected(network, 4096, activation='tanh')
+    network = fully_connected(network, 1024, activation='tanh')
     # Dropout generalizes better
     network = dropout(network, 0.5)
-    network = fully_connected(network, 4096, activation='tanh')
+    network = fully_connected(network, 1024, activation='tanh')
     network = dropout(network, 0.5)
-    network = fully_connected(network, outputs, activation='softmax')
+    network = fully_connected(network, output, activation='softmax')
 
-    network = fully_connected(network, outputs, activation='softmax')
     network = regression(network, optimizer='momentum', 
                         loss='categorical_crossentropy', 
                         learning_rate=0.001, name='target')
--- train.py
+++ train.py
@@ -9,8 +9,8 @@ with open('settings.json') as d:
     SETTINGS = json.load(d)
 
 # Our datasets
-x1, servo1 = get_servo_dataset('data/output_2.csv', start_index=35)
-x2, servo2 = get_servo_dataset('data/output_3.csv', start_index=5, end_index=520)
+x1, servo1 = get_servo_dataset('data/output_2.csv', start_index=35, output=5)
+x2, servo2 = get_servo_dataset('data/output_3.csv', start_index=5, end_index=520, output=5)
 X = x1 + x2
 SERVO = servo1 + servo2
 
@@ -19,7 +19,7 @@ SERVO = np.array(SERVO)
 
 # One NN for servo, one for motor
 # for now, outputs = 10
-servo_model = get_cnn_model(checkpoint_path='cnn_servo_model', outputs=10, **SETTINGS)
+servo_model = get_cnn_model(checkpoint_path='cnn_servo_model', output=5, **SETTINGS)
 servo_model.fit({'input': X}, {'target': SERVO}, n_epoch=10000,
                 validation_set=0.1, show_metric=True, snapshot_epoch=False,
                 snapshot_step=500, run_id='cnn_servo_model')
